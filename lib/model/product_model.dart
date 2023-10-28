2ef4ca97f07ed1d7c1eca02578e348acdab12e17
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-10-28 13:20:12 -0300

    ProductModel

---
+++ lib/model/product_model.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+
+class ProductModel extends Equatable {
+  final int quantity;
+  final int price;
+
+  const ProductModel({required this.quantity, required this.price});
+
+  factory ProductModel.fromString({
+    required String quantity,
+    required String price,
+  }) =>
+      ProductModel(
+        quantity: int.parse(quantity),
+        price: int.parse(price),
+      );
+
+  factory ProductModel.empty() => const ProductModel(quantity: 0, price: 0);
+
+  double get valueUnitary => price / quantity;
+
+  @override
+  List<Object?> get props => [quantity, price];
+}

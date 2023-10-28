7f474317b570a735951a6abfd7301c3ee2488984
LeandroRocha <leandro.brito.3@globalhitss.com.br>
2023-10-28 14:00:23 -0300

    HomeController

---
+++ lib/pages/home_controller.dart
@@ -0,0 +1,21 @@
+import '../model/product_model.dart';
+
+class HomeController {
+  ProductModel product1 = ProductModel.empty();
+  ProductModel product2 = ProductModel.empty();
+
+  static const product1Name = 'produto 1';
+  static const product2Name = 'produto 2';
+
+  String get productAdvantageous {
+    if (product1.valueUnitary < product2.valueUnitary) {
+      final proportion = product1.valueUnitary / product2.valueUnitary;
+      final percentil = (1 - proportion) * 100;
+      return 'O $product1Name é ${percentil.toStringAsFixed(1)} mais barato que o $product2Name';
+    } else {
+      final proportion = product2.valueUnitary / product1.valueUnitary;
+      final percentil = (1 - proportion) * 100;
+      return 'O $product2Name é ${percentil.toStringAsFixed(1)} mais barato que o $product1Name';
+    }
+  }
+}
